99e55c14789467cd6a5f3367610334c10125136e
Schubert1237 <yanghaotungisaac@gmail.com>
2024-08-24 01:11:45 +0800

    add and update inventory.cpp

---
+++ ExpressionsInteractivity/inventory.cpp
@@ -0,0 +1,9 @@
+/*
+    This program tracks the inventory of three widget stores that
+    opened at the same time. Each store started with the same
+    number of widgets in inventory. By subtracting the
+    number of widgets each store has sold from its inventory,
+    the current inventory can be calculated.
+*/
+#include <iostream>
+using namespace std;
