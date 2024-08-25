bf92a37ebbe77611a84c5832c6106960ff1ae157
Schubert1237 <yanghaotungisaac@gmail.com>
2024-08-25 22:25:04 +0800

    update format.cpp

--- ExpressionsInteractivity/format.cpp
+++ ExpressionsInteractivity/format.cpp
@@ -1,3 +1,9 @@
+/*
+        Display the value in radians left justified, in fixed
+        point notation, with 4 places of precision, in a field 5 spaces wide,
+        making sure the decimal point is always
+        displayed    
+    */
 #include <iostream>
 #include <iomanip>
 using namespace std; 
@@ -14,4 +20,5 @@ int main()
 
    cout << fixed << left << setprecision(4) << setw(5) << radians << endl; 
    return 0;
+   
 }
